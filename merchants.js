c920e837cdfdd29af5f842bb99068a8f18a2e1b1
devdillonhere <bizdillon@outlook.com>
2025-01-24 16:16:16 +0800

    Add files via upload

---
+++ merchants.js
@@ -0,0 +1,108 @@
+// 商户数据模板
+const merchantsData = [
+    {
+        merchantCode: "模拟商户01",
+        realName: "张明",
+        walletAddress: "0x7A3d85b95AAc57F4d7BCDA6CD5Ef288",
+        phoneNumber: "13800138001",
+        email: "zhangming@example.com",
+        address: "北京市朝阳区建国路93号院万达广场B座1208室",
+        longitude: 116.4747,
+        latitude: 39.9088
+    },
+    {
+        merchantCode: "模拟商户02",
+        realName: "李华",
+        walletAddress: "0x8B4e96c06AAd68F5d8BCEB7CD6Ef389",
+        phoneNumber: "13800138002",
+        email: "lihua@example.com",
+        address: "上海市浦东新区陆家嘴环路1318号星展银行大厦2106室",
+        longitude: 121.4978,
+        latitude: 31.2397
+    },
+    {
+        merchantCode: "模拟商户03",
+        realName: "王强",
+        walletAddress: "0x9C5f07d17BBe79F6e9CDFB8DE7Ff480",
+        phoneNumber: "13800138003",
+        email: "wangqiang@example.com",
+        address: "广州市天河区珠江新城华夏路16号富力盈凯广场3501室",
+        longitude: 113.3325,
+        latitude: 23.1200
+    },
+    {
+        merchantCode: "模拟商户04",
+        realName: "赵伟",
+        walletAddress: "0xAD6g18e28CCf80G7fADEC9EF8Gg581",
+        phoneNumber: "13800138004",
+        email: "zhaowei@example.com",
+        address: "深圳市南山区深南大道9668号华润置地大厦C座1503室",
+        longitude: 114.0579,
+        latitude: 22.5431
+    },
+    {
+        merchantCode: "模拟商户05",
+        realName: "陈杰",
+        walletAddress: "0xBE7h29f39DDg91H8gBEFDAFG9Hh682",
+        phoneNumber: "13800138005",
+        email: "chenjie@example.com",
+        address: "成都市武侯区人民南路四段27号商鼎国际2栋1802室",
+        longitude: 104.0668,
+        latitude: 30.5728
+    },
+    {
+        merchantCode: "模拟商户06",
+        realName: "杨凯",
+        walletAddress: "0xCF8i30g40EEh02I9hCFGEBGHAIi783",
+        phoneNumber: "13800138006",
+        email: "yangkai@example.com",
+        address: "武汉市江汉区解放大道688号武汉广场写字楼1204室",
+        longitude: 114.2812,
+        latitude: 30.5844
+    },
+    {
+        merchantCode: "模拟商户07",
+        realName: "刘芳",
+        walletAddress: "0xDG9j41h51FFi13J0iDGHFCHIBJj884",
+        phoneNumber: "13800138007",
+        email: "liufang@example.com",
+        address: "西安市雁塔区高新路58号光华大厦A座906室",
+        longitude: 108.8921,
+        latitude: 34.2416
+    },
+    {
+        merchantCode: "模拟商户08",
+        realName: "周鑫",
+        walletAddress: "0xEH0k52i62GGj24K1jEHIGDIJCKk985",
+        phoneNumber: "13800138008",
+        email: "zhouxin@example.com",
+        address: "南京市玄武区中山路18号德基广场2期2503室",
+        longitude: 118.7965,
+        latitude: 32.0584
+    },
+    {
+        merchantCode: "模拟商户09",
+        realName: "吴婷",
+        walletAddress: "0xFI1l63j73HHk35L2kFIJHEJKDLl086",
+        phoneNumber: "13800138009",
+        email: "wuting@example.com",
+        address: "杭州市下城区延安路530号西湖国际科技大厦B座1705室",
+        longitude: 120.1699,
+        latitude: 30.2619
+    },
+    {
+        merchantCode: "模拟商户10",
+        realName: "郑阳",
+        walletAddress: "0xGJ2m74k84IIl46M3lGJKIFKLEMm187",
+        phoneNumber: "13800138010",
+        email: "zhengyang@example.com",
+        address: "重庆市渝中区民族路188号环球金融中心3306室",
+        longitude: 106.5770,
+        latitude: 29.5591
+    }
+];
+
+// 导出数据
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = merchantsData;
+}
